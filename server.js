ec01e2313ad0c9fa8f608be2448d3330ee4ada01
Daniela Estupinan <daniela.estupinan@udla.edu.ec>
2023-05-24 08:28:21 -0500

    Server changes

--- server.js
+++ server.js
@@ -32,8 +32,9 @@ global.cryptr = new Cryptr("mySecretKey");
 
 const Filter = require("bad-words-es");
 const filter = new Filter();
+ 
 //filtro palabras adicionales
-filter.addWords('idiota','estupido','estupiada','perra');
+filter.addWords('idiota','estupido','estupida','perra','huevada','chucha','chuta');
 
 const cron = require("node-cron");
 const moment = require('moment-timezone')
