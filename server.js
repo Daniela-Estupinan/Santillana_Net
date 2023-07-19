c9bc2b98ae4bffe59430846f489f4452489eae40
Daniela Estupinan <daniela.estupinan@udla.edu.ec>
2023-07-19 15:47:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require("express");
 var app = express();
+const multer = require('multer');
+const { Storage } = require('@google-cloud/storage');
+const upload = multer({ dest: 'uploads/' });
 
 var formidable = require("express-formidable");
 app.use(formidable({
     multiples: true, // request.files to be arrays of files
 }));
 
+
+const storage = new Storage({
+	projectId: "capstone-392917", // Replace with your actual Google Cloud project ID
+	keyFilename: "capstone-392917-02f50643e575.json", // Replace with the path to your JSON keyfile
+  });
+  const bucketName = "al-dia-ecuador";
 var mongodb = require("mongodb");
 var mongoClient = mongodb.MongoClient;
 var ObjectId = mongodb.ObjectId;
@@ -1400,81 +1409,89 @@ app.post("/fetchNearbyCom", async function (request, result) {
 		app.get("/logout", function (request, result) {
 			result.redirect("/login");
 		});
-
-		app.post("/uploadCoverPhoto", function (request, result) {
-			var accessToken = request.fields.accessToken;
-			var coverPhoto = "";
-
-			database.collection("users").findOne({
-				"accessToken": accessToken
-			}, function (error, user) {
-				if (user == null) {
-					result.json({
-						"status": "error",
-						"message": "User has been logged out. Please login again."
-					});
-				} else {
-
-					if (user.isBanned) {
-						result.json({
-							"status": "error",
-							"message": "Ha sido bloqueado"
-						});
-						return false;
-					}
-
-					if (request.files.coverPhoto.size > 0 && request.files.coverPhoto.type.includes("image")) {
-
-						if (user.coverPhoto != "") {
-							fileSystem.unlink(user.coverPhoto, function (error) {
-								//
-							});
-						}
-
-						coverPhoto = "public/images/cover-" + new Date().getTime() + "-" + request.files.coverPhoto.name;
-
-						// Read the file
-	                    fileSystem.readFile(request.files.coverPhoto.path, function (err, data) {
-	                        if (err) throw err;
-	                        console.log('File read!');
-
-	                        // Write the file
-	                        fileSystem.writeFile(coverPhoto, data, function (err) {
-	                            if (err) throw err;
-	                            console.log('File written!');
-
-	                            database.collection("users").updateOne({
-									"accessToken": accessToken
-								}, {
-									$set: {
-										"coverPhoto": coverPhoto
-									}
-								}, function (error, data) {
-									result.json({
-										"status": "status",
-										"message": "Cover photo has been updated.",
-										data: mainURL + "/" + coverPhoto
-									});
-								});
-	                        });
-
-	                        // Delete the file
-	                        fileSystem.unlink(request.files.coverPhoto.path, function (err) {
-	                            if (err) throw err;
-	                            console.log('File deleted!');
-	                        });
-	                    });
-						
-					} else {
-						result.json({
-							"status": "error",
-							"message": "Please select valid image."
-						});
-					}
-				}
-			});
+//
+app.post("/uploadCoverPhoto", function (request, result) {
+	var accessToken = request.fields.accessToken;
+  
+	database.collection("users").findOne({
+	  "accessToken": accessToken
+	}, function (error, user) {
+	  if (user == null) {
+		result.json({
+		  "status": "error",
+		  "message": "User has been logged out. Please login again."
 		});
+	  } else {
+		if (user.isBanned) {
+		  result.json({
+			"status": "error",
+			"message": "Ha sido bloqueado"
+		  });
+		  return false;
+		}
+  
+		if (request.files.coverPhoto.size > 0 && request.files.coverPhoto.type.includes("image")) {
+  
+		  if (user.coverPhoto != "") {
+			// Delete the previous cover photo from Google Cloud Storage
+			const fileName = user.coverPhoto.split('/').pop();
+			const bucket = storage.bucket(bucketName);
+			bucket.file(`covers/${fileName}`).delete().catch((err) => {
+			  console.error('Error deleting previous cover photo from GCS:', err);
+			});
+		  }
+  
+		  // Upload the new cover photo to Google Cloud Storage
+		  const coverPhoto = `${request.files.coverPhoto.name}`;
+		  console.log(coverPhoto);
+		  const bucket = storage.bucket(bucketName);
+		  const blob = bucket.file(coverPhoto);
+  
+		  // Stream the file to Google Cloud Storage
+		  fileSystem.createReadStream(request.files.coverPhoto.path)
+			.pipe(blob.createWriteStream())
+			.on('error', (err) => {
+			  console.error('Error uploading cover photo to GCS:', err);
+			  result.json({
+				"status": "error",
+				"message": "An error occurred while uploading the cover photo."
+			  });
+			})
+			.on('finish', () => {
+			  // Update the user's coverPhoto field in the database
+			  database.collection("users").updateOne(
+				{ "accessToken": accessToken },
+				{ $set: { "coverPhoto": coverPhoto } },
+				
+				function (error, data) {
+				  result.json({
+					"status": "success",
+					"message": "Cover photo has been updated.",
+					data: `https://storage.googleapis.com/${bucketName}/${coverPhoto}`
+				  });
+				}
+			  );
+			});
+  
+		  // Delete the local file after uploading
+		  fileSystem.unlink(request.files.coverPhoto.path, (err) => {
+			if (err) {
+			  console.error('Error deleting local cover photo:', err);
+			} else {
+			  console.log('Local cover photo deleted!');
+			}
+		  });
+		} else {
+		  result.json({
+			"status": "error",
+			"message": "Please select a valid image."
+		  });
+		}
+	  }
+	});
+  });
 
+//
 		app.post("/uploadProfileImage", function (request, result) {
 			var accessToken = request.fields.accessToken;
 			var profileImage = "";
